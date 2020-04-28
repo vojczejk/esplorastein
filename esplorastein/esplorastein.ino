d912addd1635276be1ca7533fa89fc6f24ae3246
Vojtěch Nevřela <nevrelav@gmail.com>
2020-04-28 03:11:25 +0200

    Somwehat working fake 3d!

--- esplorastein/esplorastein.ino
+++ esplorastein/esplorastein.ino
@@ -133,29 +133,29 @@ public:
         lx = (x << SCALESHIFT) + 2;
         ly = (y << SCALESHIFT) + 2;
         cnt = 0;
-        EsploraTFT.stroke(0,0,255);
+        //EsploraTFT.stroke(0,0,255);
         while(true)
         {
-            EsploraTFT.point(ly,lx);
+            //EsploraTFT.point(ly,lx);
             if(lx > (MAXX*SCALE))
             {
-                Serial.println("LX");
+                //Serial.println("LX");
                 return distance;
             }
             if(ly > (MAXY*SCALE))
             {
-                Serial.println("LY");
+                //Serial.println("LY");
                 return distance;
             }
             if(m->at(lx >> SCALESHIFT, ly >> SCALESHIFT) != 0)
             {
-                Serial.println("COLLISION");
+                //Serial.println("COLLISION");
                 return distance;
             }
 
             if(cnt >= stepping)
             {
-                Serial.println(cnt);
+                //Serial.println(cnt);
                 switch(angle)
                 {
                     case 0:
@@ -373,8 +373,112 @@ void gameprint(Map * m, Player * p)
     Serial.println();
 }
 
+//const PROGMEM uint8_t 3drays[] = {};
 
+void vertline(uint8_t offset, uint8_t shade)
+{
+    uint8_t s = (255-shade)>>1;
+    EsploraTFT.stroke(s,s,s);
+    EsploraTFT.line(offset,128-(shade>>1),offset,shade>>1);
+}
 
+void draw3D(Player * pl)
+{
+    EsploraTFT.background(0,0,0);
+    uint8_t rays[29];
+    rays[0]  = (uint8_t)pl->ray(1,true)    ;
+    rays[1]  = (uint8_t)pl->ray(2,true)    ;
+    rays[2]  = (uint8_t)pl->ray(3,true)    ;
+    rays[3]  = (uint8_t)pl->ray(4,true)    ;
+    rays[4]  = (uint8_t)pl->ray(5,true)    ;
+    rays[5]  = (uint8_t)pl->ray(6,true)    ;
+    rays[6]  = (uint8_t)pl->ray(7,true)    ;
+    rays[7]  = (uint8_t)pl->ray(8,true)    ;
+    rays[8]  = (uint8_t)pl->ray(10,true)   ;
+    rays[9]  = (uint8_t)pl->ray(11,true)   ;
+    rays[10] = (uint8_t)pl->ray(14,true)   ;
+    rays[11] = (uint8_t)pl->ray(19,true)   ;
+    rays[12] = (uint8_t)pl->ray(29,true)   ;
+    rays[13] = (uint8_t)pl->ray(57,true)   ;
+    rays[14] = (uint8_t)pl->ray(255,true)  ;
+    rays[15] = (uint8_t)pl->ray(57,false)  ;
+    rays[16] = (uint8_t)pl->ray(29,false)  ;
+    rays[17] = (uint8_t)pl->ray(19,false)  ;
+    rays[18] = (uint8_t)pl->ray(14,false)  ;
+    rays[19] = (uint8_t)pl->ray(11,false)  ;
+    rays[20] = (uint8_t)pl->ray(10,false)  ;
+    rays[21] = (uint8_t)pl->ray(8,false)   ;
+    rays[22] = (uint8_t)pl->ray(7,false)   ;
+    rays[23] = (uint8_t)pl->ray(6,false)   ;
+    rays[24] = (uint8_t)pl->ray(5,false)   ;
+    rays[25] = (uint8_t)pl->ray(4,false)   ;
+    rays[26] = (uint8_t)pl->ray(3,false)   ;
+    rays[27] = (uint8_t)pl->ray(2,false)   ;
+    rays[28] = (uint8_t)pl->ray(1,false)   ;
+    for(uint8_t i = 0; i < 160; ++i)
+    {
+        if(i < 24)
+            vertline(i,rays[0]);
+        else if(i < 48)
+            vertline(i,rays[1]);
+        else if(i < 60)
+            vertline(i,rays[2]);
+        else if(i < 66)
+            vertline(i,rays[3]);
+        else if(i < 70)
+            vertline(i,rays[4]);
+        else if(i < 74)
+            vertline(i,rays[5]);
+        else if(i < 76)
+            vertline(i,rays[6]);
+        else if(i < 78)
+            vertline(i,rays[7]);
+        else if(i < 80)
+            vertline(i,rays[8]);
+        else if(i < 82)
+            vertline(i,rays[9]);
+        else if(i < 84)
+            vertline(i,rays[10]);
+        else if(i < 86)
+            vertline(i,rays[11]);
+        else if(i < 88)
+            vertline(i,rays[12]);
+        else if(i < 90)
+            vertline(i,rays[13]);
+        else if(i < 91)
+            vertline(i,rays[14]);
+        else if(i < 92)
+            vertline(i,rays[15]);
+        else if(i < 94)
+            vertline(i,rays[16]);
+        else if(i < 96)
+            vertline(i,rays[17]);
+        else if(i < 98)
+            vertline(i,rays[18]);
+        else if(i < 100)
+            vertline(i,rays[19]);
+        else if(i < 102)
+            vertline(i,rays[20]);
+        else if(i < 104)
+            vertline(i,rays[21]);
+        else if(i < 106)
+            vertline(i,rays[22]);
+        else if(i < 110)
+            vertline(i,rays[23]);
+        else if(i < 114)
+            vertline(i,rays[24]);
+        else if(i < 120)
+            vertline(i,rays[25]);
+        else if(i < 132)
+            vertline(i,rays[26]);
+        else if(i < 156)
+            vertline(i,rays[27]);
+        else if(i < 180)
+            vertline(i,rays[28]);
+        else
+            Serial.println("bad 3d I");
+    }
+}
 
 Map m;
 Player p(10,10,0, &m);
@@ -386,8 +490,9 @@ void setup()
     EsploraTFT.background(0,0,0);
     m.clear();
     m.randomize(20);
-    m.tftPrint();
-    p.tftPrint();
+    //m.tftPrint();
+    //p.tftPrint();
+    draw3D(&p);
 }
 
 void loop()
@@ -405,32 +510,29 @@ void loop()
           //Serial.println("UP");
           p.forward();
           gameprint(&m,&p);
-          p.tftPrint();
+          draw3D(&p);
+          //p.tftPrint();
           break;
         case SWITCH_DOWN:
           //Serial.println("DOWN");
           p.backward();
           gameprint(&m,&p);
-          p.tftPrint();
-          p.ray(255,false);
-          p.ray(1,false);
-          p.ray(2,false);
-          p.ray(3,false);
-          p.ray(1,true);
-          p.ray(2,true);
-          p.ray(3,true);
+          draw3D(&p);
+          //p.tftPrint();
           break;
         case SWITCH_LEFT:
           //Serial.println("LEFT");
           p.left();
           gameprint(&m,&p);
-          p.tftPrint();
+          draw3D(&p);
+          //p.tftPrint();
           break;
         case SWITCH_RIGHT:
           //Serial.println("RIGHT");
           p.right();
           gameprint(&m,&p);
-          p.tftPrint();
+          draw3D(&p);
+          //p.tftPrint();
           break;
         default:
           Serial.println("Bordel");
